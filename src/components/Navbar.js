7b5bb786fd781a40a411a6618dd1ec1390ababbb
ThisaraAriyawansha <thisara.a2001@gmail.com>
2024-06-02 18:57:33 +0530

    Add image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
-// Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom'; // If using React Router
 import './Navbar.css'; // Import your CSS file for navbar styles
+import logoImage from './images/3.jpg'; // Import the logo image
 
 const Navbar = () => {
     return (
@@ -10,11 +10,14 @@ const Navbar = () => {
                 <ul className="navbar-menu">
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/services">Services</Link></li>
+                    <li className="navbar-center-item">
+                        <img src={logoImage} alt="Logo" className="navbar-logo" />
+                    </li>
                     <li><Link to="/about">About</Link></li>
-                    <li><Link to="/pie-chart">Summery</Link></li>
-
+                    <li><Link to="/pie-chart">Summary</Link></li>
                     
                     {/* Add more links as needed */}
+                   
                 </ul>
             </div>
         </nav>
