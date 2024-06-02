a48ef72ddd7274a22b17316a7a14ec5212b0d0b2
ThisaraAriyawansha <thisara.a2001@gmail.com>
2024-06-02 19:26:52 +0530

    edit navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // If using React Router
 import './Navbar.css'; // Import your CSS file for navbar styles
-import logoImage from './images/3.jpg'; // Import the logo image
 
 const Navbar = () => {
     return (
@@ -11,13 +10,11 @@ const Navbar = () => {
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/services">Services</Link></li>
                     <li className="navbar-center-item">
-                        <img src={logoImage} alt="Logo" className="navbar-logo" />
+                        <i className="fas fa-balance-scale navbar-icon"></i>
                     </li>
                     <li><Link to="/about">About</Link></li>
                     <li><Link to="/pie-chart">Summary</Link></li>
-                    
                     {/* Add more links as needed */}
-                   
                 </ul>
             </div>
         </nav>
